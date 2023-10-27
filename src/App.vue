94065bebe95c6210468ce8b6e4599e1987a1bcbe
Shaun Thornburgh <personal@shaunthornburgh.com>
2023-10-27 11:23:11 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <h1 class="text-base font-semibold leading-7 text-gray-900 pt-8">Vue 3 - Props Down, Events Up Demo</h1>
+    <ChildComponent :message="parentMessage" @messageClicked="handleMessageClick" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ChildComponent from './components/ChildComponent.vue';
+const parentMessage = ref("Hello from Parent!");
+function handleMessageClick() {
+  alert('Message from child was clicked!');
+}
+</script>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="mt-2">Message from parent: {{ message }}</p>
+  <button
+      @click="sendMessageClick"
+      type="button"
+      class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-6"
+  >Click me to emit to parent component</button>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const { message } = defineProps(['message']);
+const emit = defineEmits();
+
+function sendMessageClick() {
+  emit('messageClicked');
+}
+</script>
